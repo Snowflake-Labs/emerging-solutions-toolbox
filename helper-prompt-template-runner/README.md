769692a0fa0c6e2404b791b69e503bc107b8bdef
Brett Klein <brett.klein@snowflake.com>
2025-02-06 15:23:55 -0500

    Release

---
+++ helper-prompt-template-runner/README.md
@@ -0,0 +1,236 @@
+# Cortex Prompt Template Runner
+Created by Jason Summer, *Senior Architect - AI/ML*
+
+All sample code is provided for reference purposes only. Please note that this code is provided “AS IS” and without warranty.  Snowflake will not offer any support for use of the sample code.
+
+Copyright (c) 2024 Snowflake Inc. All Rights Reserved.
+
+Please see TAGGING.md for details on object comments.
+
+# Overview
+The Prompt Template Runner enables Snowflake users to create and manage Cortex Complete calls against tables/views using a simple configuration file. 
+The Prompt Template Runner takes inspiration from LangChain [prompt templates](https://python.langchain.com/docs/concepts/prompt_templates/) and [YAML prompts](https://www.restack.io/docs/langchain-knowledge-langchain-yaml-prompt-guide), but is purpose-built to be operationalized against Snowflake table records. 
+Here, the key difference is that prompt variables may be imputed by literal constants (similar to Langchain) OR column values from the underlying table.
+
+The helper provides 2 utilities to operationalize templated prompts against Snowflake tables:
+
+1) **PROMPT_TEMPLATE_PARSER**: User Defined Table Function (UDTF) to create a multi-message prompt from every record in a table, replacing variables with literal and/or table values.
+2) **PROMPT_TEMPLATE_RUNNER**: Stored Procedure to run a prompt template through the PROMPT_TEMPLATE_PARSER and prompt an LLM.
+
+# Specifications
+The configuration file currently supports the below elements in the top-level `prompt` key.
+
+| Element | Description |
+|---------|-------------|
+| `name` | Name of the prompt template. |
+| `version` | Version of the prompt template. |
+| `messages` | List of objects containing role and content keys to serve as multi-message prompt to Cortex. |
+| `literal_variables` | Objects mapping variable and **constant** replacement value in prompt. |
+| `column_variables` | Objects mapping variable and **column** replacement value in prompt. |
+| `origin_table` | Table against which Cortex Complete will be called. |
+| `model` | Cortex Complete [model string](https://docs.snowflake.com/en/sql-reference/functions/complete-snowflake-cortex#arguments). |
+| `model_options` | Object specifiying zero or more [model hyperparameters](https://docs.snowflake.com/en/sql-reference/functions/complete-snowflake-cortex#arguments). |
+
+Here is an example of a YAML prompt configuration file:
+```yaml
+prompt:
+  name: "Review Sentiment Checker"
+  version: "1.0"
+  messages:
+    - role : "system"
+      content: |
+        You are a helpful marketing assistant. 
+        You will be given a movie review and sentiment 
+        and must determine if the sentiment is accurate.
+        Your responses should be {format}.
+    - role : "user"
+      content: |
+        Here is the review: {review}.
+        Here is the sentiment: {sentiment}.
+  literal_variables:
+    format: "concise"
+  column_variables:
+    review: "REVIEW"
+    sentiment: "SENTIMENT"
+  origin_table: 'JSUMMER.SAMPLE_DATA.MOVIES_LIMITED'
+  model: 'llama3.2-3b'
+  model_options: 
+    max_tokens: 100
+    temperature: 0.5
+```
+
+## Messages
+Messages contains the prompt or history of messages. The argument must be an array of objects representing a conversation in chronological order. Each object must contain a `role` key and a `content` key. The `content` value is a prompt or a response, depending on the role. The role must be one of the following: `system`, `user`, `assistant`. See the [Cortex Complete documentation](https://docs.snowflake.com/en/sql-reference/functions/complete-snowflake-cortex) for more information. 
+
+If passing only a single message, set `role` as 'user'.
+
+## Variables
+Variables are any parts of the prompts/messages that should be replaced for every record in the table.
+These variables can be replaced (e.g. imputed) by a constant, meaning every table record will feature the same value in the prompt. Alternatively, a variable can be replaced (e.g. imputed) by a column value in the corresponding record. 
+
+> **Important:** Variables must be enclosed in brackets in the prompts/messages.
+
+For example, you may have a table with columns `PRODUCT_NAME` and `PRODUCT_DESCRIPTION` and want to create a slogan for each one based on a consistent theme. Your prompt could be something like the below.
+
+```yaml
+...
+  messages:
+    - role : "user"
+      content: |
+        Create a slogan for this product: {product_name}
+        Here is the description:
+        {description}
+        The slogan should include themes from {theme}.
+```
+
+And your variables section could like the below.
+```yaml
+...
+  literal_variables:
+    theme: "baseball"
+  column_variables:
+    product_name: "PRODUCT_NAME"
+    description: "PRODUCT_DESCRIPTION"
+```
+
+# Setup as Stored Procedure
+The utilities (e.g. the UDTF and Stored Procedures) can be registered directly by using Snowflake's git integration. Below are a couple options to do so.
+
+## Snowsight Worksheet
+Copy and paste the contents of `register.sql` into a Snowsight SQL Worksheet. Ensure your context role is appropriate as this will be the owning role of the corresponding objects. Execute the entire SQL Worksheet.
+
+## VSCode with Snowflake Extension
+See [Snowflake Extension for Visual Studio Code installation documentation](https://docs.snowflake.com/en/user-guide/vscode-ext) for instructions. Once installed, sign into Snowflake in the extension. Execute all of `register.sql` from VSCode. Ensure your context role is appropriate as this will be the owning role of the corresponding objects.
+
+# Running
+The configuration file is passed as a parameter when invoking the utilities.
+The file should be in Snowflake Stage.
+
+## PROMPT_TEMPLATE_PARSER
+
+The PROMPT_TEMPLATE_PARSER is a UDTF and should be called against a Snowflake table.
+A configuration file in Snowflake stage can be passed to the UDTF OR an explicit object containing the same arguments can be passed. 
+
+### Examples
+
+Calling PROMPT_TEMPLATE_PARSER using a configuration file in stage. 
+```sql
+WITH CTE
+AS (
+    SELECT 
+        *,
+        OBJECT_CONSTRUCT(*) AS ROW_DICT, -- Necessary to pass row values to prompt
+        BUILD_SCOPED_FILE_URL('@JSUMMER.PUBLIC.DROPBOX','prompt_template.yaml') AS CONFIG_FILE -- Always used BUILD_SCOPED_FILE_URL
+    FROM JSUMMER.SAMPLE_DATA.MOVIES_LIMITED -- Sample table
+)
+SELECT
+    * EXCLUDE (ROW_DICT, CONFIG_FILE) -- Don't need to include these extra columns
+FROM CTE
+, TABLE(GENAI_UTILITIES.UTILITIES.PROMPT_TEMPLATE_PARSER(
+        row_data => ROW_DICT, 
+        prompt_template_file => CONFIG_FILE,
+        include_metadata => TRUE -- Set to FALSE to just get messages array back without metadata
+        ))
+```
+
+```python
+import snowflake.snowpark.functions as F
+
+origin_table = 'JSUMMER.SAMPLE_DATA.MOVIES_LIMITED'
+prompt_column = 'PROMPT'
+
+# Must first create object construct column as separate request to avoid selection error
+df = session.table(origin_table).with_column('ROW_DATA', F.object_construct('*'))
+
+# Calling the UDTF using call_table_function from snowpark.functions
+df = df.with_column(prompt_column, F.call_table_function(
+        'GENAI_UTILITIES.UTILITIES.PROMPT_TEMPLATE_PARSER',
+        F.col('ROW_DATA'),
+        F.call_builtin('BUILD_SCOPED_FILE_URL','@JSUMMER.PUBLIC.DROPBOX','prompt_template.yaml'),
+        F.parse_json(F.lit(None)), # Can omit if using a configuration file
+        F.lit(True) # Can omit and will default to False
+        )).drop('ROW_DATA')
+df.show()
+```
+
+Calling PROMPT_TEMPLATE_PARSER without a configuration file and instead passing the parameters in-line.
+```python
+import snowflake.snowpark.functions as F
+
+config = {
+    "name": "Review Sentiment Checker",
+    "version": "1.0",
+    "messages": [
+        {
+            "role": "system",
+            "content": "You are a helpful marketing assistant. "
+                       "You will be given a movie review and sentiment "
+                       "and must determine if the sentiment is accurate. "
+                       "Your responses should be {format}."
+        },
+        {
+            "role": "user",
+            "content": "Here is the review: {review}. "
+                       "Here is the sentiment: {sentiment}."
+        }
+    ],
+    "literal_variables": {
+        "format": "concise"
+    },
+    "column_variables": {
+        "review": "REVIEW",
+        "sentiment": "SENTIMENT"
+    },
+}
+
+origin_table = 'JSUMMER.SAMPLE_DATA.MOVIES_LIMITED'
+prompt_column = 'PROMPT'
+
+# Must first create object construct column as separate request to avoid selection error
+df = session.table(origin_table).with_column('ROW_DATA', F.object_construct('*'))
+
+# Calling the UDTF using call_table_function from snowpark.functions
+df = df.with_column(prompt_column, F.call_table_function(
+        'GENAI_UTILITIES.UTILITIES.PROMPT_TEMPLATE_PARSER',
+        F.col('ROW_DATA'),
+        F.lit(''),
+        F.to_variant(F.lit(config)),
+        F.lit(False))).drop('ROW_DATA')
+df.show()
+```
+## PROMPT_TEMPLATE_RUNNER
+
+The PROMPT_TEMPLATE_RUNNER is a Stored Procedure that can be executed standalone with all necessary components driven from a configuration file.
+The below arguments can be passed explicitly to the utility in addition to the configuration file.
+Arguments passed explicitly will be prioritized over those in the configuration file.
+- `origin_table`
+- `model`
+- `model_options`
+
+### Examples
+
+Calling the stored procedure relying on the configuration file for all arguments.
+```sql
+CALL GENAI_UTILITIES.UTILITIES.PROMPT_TEMPLATE_RUNNER(BUILD_SCOPED_FILE_URL('@JSUMMER.PUBLIC.DROPBOX', 'prompt_template.yaml'));
+```
+
+Calling the stored procedure and passing explicit arguments to override those in the configuration file.
+```sql
+CALL GENAI_UTILITIES.UTILITIES.PROMPT_TEMPLATE_RUNNER(
+    prompt_template_file => BUILD_SCOPED_FILE_URL('@JSUMMER.PUBLIC.DROPBOX', 'prompt_template.yaml'),
+    origin_table => 'JSUMMER.SAMPLE_DATA.MOVIES_LIMITED',
+    model => 'llama3.2-1b',
+    model_options => {'temperature': 0.1, 
+                      'max_tokens': 90}
+    );
+```
+
+
+# Metadata Tracking
+The below metadata elements from the prompt template (or explicitly passed) are added to the Cortex Complete response object:
+- name
+- version
+- model_options
+
+# Feedback
+Please add issues to GitHub or email Jason Summer (jason.summer@snowflake.com).
---
+++ helper-prompt-template-runner/prompt_parser.py
@@ -0,0 +1,374 @@
+from typing import Optional, Union, Any
+import datetime
+
+import snowflake.snowpark.functions as F
+from snowflake.snowpark import DataFrame
+from snowflake.snowpark import Session
+
+major, minor = 1, 1
+QUERY_TAG = {
+    "origin": "sf_sit",
+    "name": "prompt_template_runner",
+    "version": {"major": major, "minor": minor},
+}
+
+def set_query_tag(session: Session) -> None:
+    """
+    Sets the query tag for the Snowflake session.
+    Args:
+        session (Session): The Snowflake session object.
+        
+    Returns:
+        None
+    """
+    try:
+        session.query_tag = QUERY_TAG
+    except Exception:
+        return
+    
+class PromptParser:
+    def __init__(self):
+        self.configuration_loaded = False
+        self.prompt_config = None
+
+    def process(self, row_data: dict, prompt_template_file: str, prompt_config: dict, include_metadata: bool = False):
+        if not self.configuration_loaded:
+            if prompt_template_file:
+                self.prompt_config = extract_prompt(prompt_template_file)
+            else:
+                self.prompt_config = prompt_config
+ 
+            if not self.prompt_config:
+                raise ValueError(f"Error: prompt_config is required in configuration file or explicitly.")
+            self.configuration_loaded = True
+        
+        messages = self.prompt_config.get('messages', [])
+        literal_vars = self.prompt_config.get('literal_variables', {})
+        column_vars = self.prompt_config.get('column_variables', {})
+        prompt_name = self.prompt_config.get('name', str(datetime.date.today()))
+        prompt_version = self.prompt_config.get('version', str(datetime.datetime.now().time()))
+            
+        # Replace column names with column values in column_variables
+        mapped_column_vals = {} # Make new mapping in case we need to exlude missing row data
+        if column_vars:
+            for var_name, col_name in column_vars.items():
+                if col_name in row_data:
+                    mapped_column_vals[var_name] = row_data.get(col_name, '')
+    
+        replace_dict = {**(literal_vars or {}), **(mapped_column_vals or {})} # Combine dictionaries if not empty
+
+        # Process messages
+        updated_messages = []
+        if messages:
+            for message in messages:
+                updated_message = {}
+                for key, value in message.items():
+                    mapped_value = value
+                    for source_value, target_value in replace_dict.items():
+                        mapped_value = mapped_value.replace(f'{{{source_value}}}', target_value)
+                    updated_message[key] = mapped_value
+                updated_messages.append(updated_message)
+    
+        # Yield as a tuple, since UDTFs must return tuples
+        if include_metadata:
+            yield ({'messages': updated_messages, 
+                    'prompt_name': prompt_name, 
+                    'prompt_version': prompt_version},)
+        else:
+            yield (updated_messages,)
+
+    
+def test_model(
+        session: Session, 
+        model: str, 
+        prompt = "Repeat the word hello once and only once. Do not say anything else."
+        ) -> bool:
+    from snowflake.cortex import Complete
+    from snowflake.snowpark.exceptions import SnowparkSQLException
+
+    """Returns True if selected model is supported in region and returns False otherwise.
+    
+    Args:
+        session (Session): The Snowflake session object.
+        model (str): The model to use for the SNOWFLAKE.CORTEX.COMPLETE function.
+        prompt (str): The prompt to test the model with. 
+            Defaults to "Repeat the word hello once and only once. Do not say anything else."
+    """
+    try:
+        response = Complete(model, prompt, session = session)
+        return True
+    except SnowparkSQLException as e:
+        if 'unknown model' in str(e):
+            return False
+
+def run_complete_options(
+        session: Session,
+        source_table: str,
+        model: str,
+        model_options: dict[str, Union[str, float, int]] = {},
+        prompt_column: str = 'PROMPT',
+        response_column: str = 'RESPONSE',
+        ) -> DataFrame:
+    """
+    Executes a SQL query to run the SNOWFLAKE.CORTEX.COMPLETE function on a specified table.
+    Args:
+        session (Session): The Snowflake session object.
+        source_table (str): The name of the source table to query.
+        model (str): The model to use for the SNOWFLAKE.CORTEX.COMPLETE function.
+        model_options (Optional[dict[str, Union[str, float, int]]]): Options for the model in a dictionary format. 
+            Defaults to an empty dictionary.
+        prompt_column (Optional[str]): The name of the column containing the prompts. Defaults to 'PROMPT'.
+        response_column (Optional[str]): The name of the column to store the responses. Defaults to 'RESPONSE'.
+    Returns:
+        DataFrame: The result of the SQL query as a DataFrame.
+    Raises:
+        Exception: If there is an error executing the SQL query.
+    """
+
+    import json
+    
+    query = f"""
+        SELECT * EXCLUDE ({prompt_column}), 
+        SNOWFLAKE.CORTEX.COMPLETE(
+        ?,
+            {prompt_column}, PARSE_JSON(?)
+            ) as {response_column}          
+        FROM {source_table}
+        """
+    
+    # If error occurs, immediately raise exception as error may be across multiple records
+    try:
+        return session.sql(
+            query.format(
+                prompt_column = prompt_column,
+                response_column = response_column,
+                source_table = source_table
+                    ), 
+            params = [
+                model,
+                json.dumps(model_options),
+                ]
+            )
+    except Exception as e:
+        raise Exception(f"Error running Cortex Complete query: {e}")
+
+def parse_prompt_template(
+        session: Session,
+        prompt_config: dict[str, Any],
+        origin_table: Optional[str] = None,
+        output_table_name: Optional[str] = 'TEMP',
+        table_type: Optional[str] = 'temporary',
+        table_write_mode: str = 'errorifexists',
+        prompt_column: str = 'PROMPT'
+        ) -> None:
+    """
+    Parses a prompt template and saves the results to a specified table.
+    Args:
+        session (Session): The Snowflake session object.
+        origin_table (str): The name of the origin table to read data from.
+        prompt_config (dict[str, Any]): Prompt template dictionary.
+        output_table_name (Optional[str]): The name of the output table. Defaults to 'TEMP'.
+        table_type (Optional[str]): The type of the output table (temporary, transient, or permanent).
+            Defaults to 'temporary'.
+        table_write_mode (Optional[str]): The mode to write the table (append, overwrite, truncate, errorifexists, ignore). 
+            Defaults to 'errorifexists'.
+        prompt_column (Optional[str]): The column name for the prompt. Defaults to 'PROMPT'.
+    Returns:
+        None
+        Saves dataframe to Snowflake table.
+    """
+
+    if origin_table is None:
+        origin_table = prompt_config.get('origin_table', None)
+
+    # Final check for required parameters
+    if origin_table is None:
+        raise ValueError(f"Error: origin_table is required in configuration file or explicitly.")
+    
+    # Must first create object construct column as separate request to avoid selection error
+    df = session.table(origin_table).with_column('ROW_DATA', F.object_construct('*'))
+
+    # Calling the UDTF using call_table_function from snowpark.functions
+    udtf = 'GENAI_UTILITIES.UTILITIES.PROMPT_TEMPLATE_PARSER'
+
+    # We set metadata to exclude so we can add it in once later in stored procedure
+    df = df.with_column(prompt_column, F.call_table_function(
+            udtf,
+            F.col('ROW_DATA'),
+            F.lit(''),
+            F.to_variant(F.lit(prompt_config)),
+            F.lit(False))).drop('ROW_DATA')
+    
+    # Save results to table
+    if output_table_name is None:
+        output_table_name = prompt_config['name'].replace(' ', '_').upper()
+    if table_type not in ['temporary', 'transient', '']:
+        table_type = 'temporary' # Default to temporary table
+    if table_write_mode not in ['append', 'overwrite', 'truncate', 'errorifexists', 'ignore']:
+        table_write_mode = 'errorifexists' # Default to error before overriding
+    df.write.mode(table_write_mode).save_as_table(output_table_name, table_type=table_type)
+
+def extract_prompt(prompt_template_file: str) -> dict[str, Any]:
+    """
+    Extracts the prompt from a YAML file.
+
+    Uses SnowflakeFile if the file is a URL corresponding to Snowflake stage.
+    Otherwise, uses the standard Python open function for a local file.
+
+    Args:
+        prompt_template_file (str): The path to the YAML file containing the prompt template.
+    Returns:
+        dict[str, Any]: The prompt dictionary.
+    """
+
+    import yaml
+    
+    if prompt_template_file.startswith('https://'): # BUILD_SCOPED_FILE_URL returns URL
+        from snowflake.snowpark.files import SnowflakeFile
+        open_file = SnowflakeFile.open(prompt_template_file)
+    else:
+        open_file = open(prompt_template_file, 'r')
+        
+    with open_file as file:
+        try:
+            return yaml.safe_load(file)['prompt']
+        except KeyError:
+            raise KeyError(f"Error parsing YAML file {prompt_template_file}. Ensure that the file contains a 'prompt' key.")
+        except yaml.YAMLError as e:
+            raise yaml.YAMLError(f"Error parsing YAML file {prompt_template_file}. Error: {e}")
+        
+def add_metadata(df: DataFrame, column: str, metadata: dict[str, Any]) -> DataFrame:
+    """
+    Adds metadata to a specified column in the DataFrame. 
+    
+    If the metadata contains nested dictionaries, they are unnested and added as separate keys.
+    
+    Args:
+        df (DataFrame): The DataFrame to which metadata will be added.
+        column (str): The name of the column to which metadata will be added.
+        metadata (dict[str, Any]): A dictionary containing metadata to be added. Nested dictionaries are supported.
+    Returns:
+        DataFrame: The DataFrame with the added metadata.
+    """
+    
+    try:
+        for key, value in metadata.items():
+                if value is None:
+                    continue
+                if isinstance(value, dict): # Unnest nested dictionaries to add separate keys
+                    for sub_k, sub_v in value.items():
+                        df = df.with_column(
+                                column,
+                                F.sql_expr(f"OBJECT_INSERT({column}, '{sub_k}', '{sub_v}')")
+                            )
+                else:
+                    df = df.with_column(
+                        column,
+                        F.sql_expr(f"OBJECT_INSERT({column}, '{key}', '{value}')")
+                    )
+        return df
+    except Exception as e:
+        raise Exception(f"Error adding metadata to DataFrame: {e}")
+
+def run_prompt_template(
+        session: Session,
+        prompt_template_file: str,
+        origin_table: Optional[str] = None,
+        model: Optional[str] = None,
+        model_options: Optional[dict] = None,
+        response_column: str = 'RESPONSE',
+        output_table_name: Optional[str] = 'TEMP',
+        table_type: Optional[str] = 'temporary',
+        table_write_mode: str = 'overwrite',
+        prompt_column: str = 'PROMPT'
+        ) -> DataFrame:
+    
+    """
+    Executes a prompt template by extracting the prompt configuration, 
+        preparing the prompt table, running the model with the specified options, 
+        and adding metadata to the response.
+    
+    Args:
+        session (Session): The Snowflake session object.
+        prompt_template_file (str): The file path to the prompt template configuration.
+            If the file is in stage, use BUILD_SCOPED_FILE_URL to get the URL.
+            If the file is local, use the local file path.
+        origin_table (Optional[str]): The name of the origin table containing the prompts. 
+            If None, it will be extracted from the prompt configuration.
+        model (Optional[str]): The model to use for generating responses. 
+            If None, it will be extracted from the prompt configuration.
+        model_options (Optional[dict]): Additional options to pass to the model. 
+            If None or empty, it will be extracted from the prompt configuration.
+            model_options are optional in Cortex calls.
+        response_column (str): The name of the column to store the model responses. Default is 'RESPONSE'.
+        output_table_name (Optional[str]): The name of the output table to create. Default is 'TEMP'.
+        table_type (Optional[str]): The type of the table to create (e.g., 'temporary'). Default is 'temporary'.
+        table_write_mode (str): The mode to use when writing the output_table_name table (e.g., 'overwrite'). Default is 'overwrite'.
+        prompt_column (str): The name of the column to write prepared prompts to in output_table_name. Default is 'PROMPT'.
+            Column will not be included in output table. 
+            Column name should not conflict wiht existing columns in origin_table.
+    
+    Returns:
+        DataFrame: The DataFrame containing the model responses with added metadata.
+    Raises:
+        ValueError: If required parameters (origin_table or model) are missing.
+    """
+
+    # Set query tag for usage
+    set_query_tag(session)
+
+    prompt_config = extract_prompt(prompt_template_file)
+
+    # Options passed in signature will take priority
+    # Secondarily, we will use the prompt_config values
+    if origin_table is None:
+        origin_table = prompt_config.get('origin_table', None)
+    if model is None:
+        model = prompt_config.get('model', None)
+
+    # Check for required parameters
+    for param in [origin_table, model]:
+        if param is None:
+            raise ValueError(f"Error: origin_table and model are required in configuration file or explicitly.")
+
+    # Check model availability
+    available = test_model(session, model)
+
+    if not available:
+        raise ValueError(f"Error: Model {model} is not available in the current region.")
+
+    if isinstance(model_options, dict) and len(model_options) > 0:
+        model_options = model_options
+    else:
+        model_options = prompt_config.get('model_options', {})
+    
+    # Creates table with prompt column so we can use SQL complete with options against prepped prompt
+    parse_prompt_template(
+        session,
+        prompt_config,
+        origin_table,
+        output_table_name,
+        table_type,
+        table_write_mode,
+        prompt_column
+    )
+
+    # Run complete with options to add the response column
+    df = run_complete_options(
+        session,
+        output_table_name,
+        model,
+        model_options,
+        prompt_column,
+        response_column
+    )
+    
+    metadata = {
+        'prompt_name': prompt_config.get('name', ''),
+        'prompt_version': prompt_config.get('version', ''),
+        'model_options': model_options
+    }
+
+    df = add_metadata(df, response_column, metadata)
+
+    return df
