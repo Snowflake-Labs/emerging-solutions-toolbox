77bf69c21c01cdb254977b07475c77be597a90fb
Tyler White <tyler.white@snowflake.com>
2025-04-10 11:04:58 -0400

    doc: add found in toolbox to README

--- sfguide-getting-started-with-api-enrichment-framework/README.md
+++ sfguide-getting-started-with-api-enrichment-framework/README.md
@@ -7,10 +7,6 @@
 The API Enrichment Framework is a Native App that has been pre-set with infrastructure to allow the Consumer to enrich data in their account from an API. The Framework also includes a SiS manager app to help the provider configure the API settings.
 A consumer can download the app, enter credentials, and enrich any table with the application.
 
-Copyright (c) 2025 Snowflake Inc. All Rights Reserved.
-
-Please see TAGGING.md for details on object comments.
-
 ## Overview
 
 This framework utilizes Native Apps, The python permissions API, external functions, and serverless tasks to give a consumer easy access to any external api. 
--- sfguide-getting-started-with-api-enrichment-framework/code_artifacts/streamlit/streamlit.py
+++ sfguide-getting-started-with-api-enrichment-framework/code_artifacts/streamlit/streamlit.py
@@ -9,7 +9,6 @@ import pandas as pd
 import json
 
 
-
 if "layout" not in st.session_state:
     st.session_state.layout = "wide"
     st.set_page_config(layout=st.session_state.layout)
@@ -17,44 +16,43 @@ if "layout" not in st.session_state:
 
 session = get_active_session()
 
-QUERY_TAG = {"origin": "sf_sit",
-             "name": "api_enrichment_framework",
-             "version": '{major: 1, minor: 0}'
-            }
+QUERY_TAG = {
+    "origin": "sf_sit",
+    "name": "api_enrichment_framework",
+    "version": "{major: 1, minor: 0}",
+}
 
 
 def sql_to_dataframe(sql: str) -> pd.DataFrame:
     return session.sql(sql).collect(
-        statement_params={
-            "QUERY_TAG": json.dumps(QUERY_TAG)
-        }
+        statement_params={"QUERY_TAG": json.dumps(QUERY_TAG)}
     )
 
 
 def sql_to_pandas(sql: str) -> pd.DataFrame:
     return session.sql(sql).to_pandas(
-        statement_params={
-            "QUERY_TAG": json.dumps(QUERY_TAG)
-        }
+        statement_params={"QUERY_TAG": json.dumps(QUERY_TAG)}
     )
 
+
 dates_chron_dict = {
-            "Hourly": "0 * * * *",
-            "Daily":"0 1 * * *", 
-            "Weekly": "0 1 * * 1", 
-            "Monthly":"0 1 1 * *",
-            "Annually":"0 1 1 1 *"
-        }
+    "Hourly": "0 * * * *",
+    "Daily": "0 1 * * *",
+    "Weekly": "0 1 * * 1",
+    "Monthly": "0 1 1 * *",
+    "Annually": "0 1 1 1 *",
+}
 
 
 def set_page(page: str):
     st.session_state.page = page
 
+
 class Page(ABC):
     @abstractmethod
     def __init__(self):
         pass
-    
+
     @abstractmethod
     def print_page(self):
         pass
@@ -67,9 +65,9 @@ class Page(ABC):
 class Base_Page(Page):
     def __init__(self):
         pass
-    
+
     def print_page(self):
-        st.title('Data Enrichment Framework')
+        st.title("Data Enrichment Framework")
         pass
 
     def print_sidebar(self):
@@ -78,25 +76,30 @@ class Base_Page(Page):
             st.write(app_name)
             pass
 
+
 class main_page(Base_Page):
     def __init__(self):
-        self.name="main"
+        self.name = "main"
 
     def print_page(self):
         super().print_page()
 
         st.write("Welcome to the Data Enrichment Framework!")
 
-        scheduled_jobs = sql_to_pandas("SELECT * FROM DATA.SCHEDULED_TASKS ORDER BY CREATED_DATETIME")
+        scheduled_jobs = sql_to_pandas(
+            "SELECT * FROM DATA.SCHEDULED_TASKS ORDER BY CREATED_DATETIME"
+        )
 
         secret_associations = permissions.get_reference_associations("CONSUMER_SECRET")
 
-        external_access_associations = permissions.get_reference_associations("MY_EXTERNAL_ACCESS")
+        external_access_associations = permissions.get_reference_associations(
+            "MY_EXTERNAL_ACCESS"
+        )
 
         if len(secret_associations) == 0 or len(external_access_associations) == 0:
             st.warning("The app has not been setup yet, redirecting to settings page")
-            set_page('settings')
-            st.experimental_rerun()
+            set_page("settings")
+            st.rerun()
 
         if len(secret_associations) != 0 and len(external_access_associations) != 0:
             sql_to_dataframe("CALL SRC.INIT()")
@@ -108,17 +111,18 @@ class main_page(Base_Page):
             for index, task in scheduled_jobs.iterrows():
                 Scheduled_Task(task)
 
-        st.button("Enrich Data", on_click=set_page, args=('pick_table',))
-
+        st.button("Enrich Data", on_click=set_page, args=("pick_table",))
 
     def print_sidebar(self):
         with st.sidebar:
             super().print_sidebar()
             st.button("Settings", on_click=set_page, args=("settings",))
 
+
 class settings_page(Base_Page):
     def __init__(self):
-        self.name="settings"
+        self.name = "settings"
+
     def print_page(self):
         super().print_page()
 
@@ -126,11 +130,13 @@ class settings_page(Base_Page):
 
         secret_associations = permissions.get_reference_associations("CONSUMER_SECRET")
 
-        external_access_associations = permissions.get_reference_associations("MY_EXTERNAL_ACCESS")
+        external_access_associations = permissions.get_reference_associations(
+            "MY_EXTERNAL_ACCESS"
+        )
 
         st.write("First, please add an api key, and approve the api call endpoint")
 
-        col1,col2,__ = st.columns((2,2,10))
+        col1, col2, __ = st.columns((2, 2, 10))
 
         if col1.button("Set API Key"):
             permissions.request_reference("CONSUMER_SECRET")
@@ -144,13 +150,23 @@ class settings_page(Base_Page):
         st.divider()
 
         st.write("Second, this app needs to write out your data!")
-        st.write("Please give the permissions to create a database, and tasks so we can schedule your jobs!")
+        st.write(
+            "Please give the permissions to create a database, and tasks so we can schedule your jobs!"
+        )
         if st.button("Grant Permissions"):
-            permissions.request_account_privileges(["CREATE DATABASE", "EXECUTE MANAGED TASK"])
+            permissions.request_account_privileges(
+                ["CREATE DATABASE", "EXECUTE MANAGED TASK"]
+            )
 
-        if len(secret_associations) != 0 and len(external_access_associations) != 0 and permissions.get_held_account_privileges(["CREATE DATABASE", "EXECUTE MANAGED TASK"]):
+        if (
+            len(secret_associations) != 0
+            and len(external_access_associations) != 0
+            and permissions.get_held_account_privileges(
+                ["CREATE DATABASE", "EXECUTE MANAGED TASK"]
+            )
+        ):
             st.success("The application is configured!")
-            st.button("Go To Main Page", on_click = set_page, args=("main",))
+            st.button("Go To Main Page", on_click=set_page, args=("main",))
 
     def print_sidebar(self):
         with st.sidebar:
@@ -158,34 +174,36 @@ class settings_page(Base_Page):
 
 
 class table_mapping_page(Base_Page):
-    
+
     def __init__(self):
-        self.name="pick_table"
-    
+        self.name = "pick_table"
+
     def print_page(self):
         super().print_page()
-
 
         clicked = st.button("Pick Table")
         if clicked:
             permissions.request_reference("enrichment_table")
 
-
-        reference_associations = permissions.get_reference_associations("enrichment_table")
+        reference_associations = permissions.get_reference_associations(
+            "enrichment_table"
+        )
         tables = {}
-        if len(reference_associations) == 0:    
+        if len(reference_associations) == 0:
             st.write("No tables chosen yet! please pick a table")
         else:
 
             for table in reference_associations:
 
-                sqlstring = f"""show columns in table reference('enrichment_table','{table}')"""
+                sqlstring = (
+                    f"""show columns in table reference('enrichment_table','{table}')"""
+                )
 
                 describe_data = pd.DataFrame(sql_to_dataframe(sqlstring))
 
-                columns = describe_data['column_name']
+                columns = describe_data["column_name"]
 
-                table_name = describe_data['table_name'][0]
+                table_name = describe_data["table_name"][0]
 
                 tables[table_name] = {}
                 tables[table_name]["columns"] = list(columns)
@@ -197,16 +215,19 @@ class table_mapping_page(Base_Page):
             st.session_state.pick_table = tables[pick_table]
             # st.session_state.pick_reference = tables[pick_table]["reference"]
             # st.session_state.pick_columns = tables[pick_table]["columns"]
-            st.button("Next",key = 'page button', on_click=set_page,args=('two',))
+            st.button("Next", key="page button", on_click=set_page, args=("two",))
 
     def print_sidebar(self):
         with st.sidebar:
             super().print_sidebar()
-            st.button("Go to main",key='sidebar button', on_click=set_page,args=('main',))
+            st.button(
+                "Go to main", key="sidebar button", on_click=set_page, args=("main",)
+            )
+
 
 class page_two(Base_Page):
     def __init__(self):
-        self.name="two"
+        self.name = "two"
 
     def save_and_schedule(self):
         pick_table = st.session_state.pick_table
@@ -218,11 +239,10 @@ class page_two(Base_Page):
         pick_table["wh_size"] = wh_size
         pick_table["task_name"] = task_name
 
-
-        task_id = sql_to_dataframe("SELECT DATA.TASK_ID_SEQUENCE.NEXTVAL")[0][0]        
+        task_id = sql_to_dataframe("SELECT DATA.TASK_ID_SEQUENCE.NEXTVAL")[0][0]
         # st.write(task_id)
 
-        pick_table = str(pick_table).replace("'",'"')
+        pick_table = str(pick_table).replace("'", '"')
 
         insert_sql = f"""INSERT INTO DATA.SCHEDULED_TASKS(task_id, job_specs, CREATED_DATETIME) SELECT '{task_id}',PARSE_JSON('{pick_table}'),current_timestamp"""
 
@@ -230,43 +250,53 @@ class page_two(Base_Page):
 
         sql_to_dataframe(str(insert_sql))
 
-        sql_to_dataframe(f"""
+        sql_to_dataframe(
+            f"""
         CREATE OR REPLACE TASK DATA.{task_name}_ENRICHMENT_TASK
         USER_TASK_MANAGED_INITIAL_WAREHOUSE_SIZE = '{wh_size}'
         SCHEDULE = 'USING CRON {sched} America/Los_Angeles'
         COMMENT = 'sit_api_enrichment_framework'
         AS
             CALL SRC.ENRICH_DATA_WRAPPER({task_id})
-            """)
+            """
+        )
         st.success("Task Saved!")
         pass
 
-    def enrich(self,pick_table):
+    def enrich(self, pick_table):
         enriched_data = sql_to_dataframe(f"CALL SRC.ENRICH_DATA({pick_table})")
-        st.success('Enrichment Successfull!')
+        st.success("Enrichment Successfull!")
         # st.write(enriched_data)
 
     def print_page(self):
         # super().print_page()
         # fields = ["street_address", "city", "region", "postal_code", "iso_country_code"]
-        fields = sql_to_dataframe("SELECT VALUE FROM DATA.METADATA WHERE KEY = 'FIELDS'")
+        fields = sql_to_dataframe(
+            "SELECT VALUE FROM DATA.METADATA WHERE KEY = 'FIELDS'"
+        )
         # st.write(st.session_state.pick_table)
 
         pick_table = st.session_state.pick_table
 
         columns = pick_table["columns"]
         table = pick_table["reference"]
-        
+
         st.subheader("Map Data")
 
         map_dict = {}
 
         if len(fields) == 0:
-            st.warning("There are no fields, please have the admin configure the fields")
+            st.warning(
+                "There are no fields, please have the admin configure the fields"
+            )
         else:
-            fields = json.loads(fields[0][0])["fields"]     
+            fields = json.loads(fields[0][0])["fields"]
             for field in fields:
-                map_dict[field] = st.selectbox(f"Pick a column to map ***{field}*** to", columns, key="pick_"+field)
+                map_dict[field] = st.selectbox(
+                    f"Pick a column to map ***{field}*** to",
+                    columns,
+                    key="pick_" + field,
+                )
 
         sqlstring = f"""select top 10 * from reference('enrichment_table','{table}')"""
         address_data = sql_to_pandas(sqlstring)
@@ -283,19 +313,26 @@ class page_two(Base_Page):
 
         output_table = st.text_input("Output Table")
 
-
         if output_table:
-            pick_table["output_table"] = output_table            
-            col1,col2,__ = st.columns((2,2,20))
-            col1.button("Save", key = 'enrich', on_click=self.enrich,args=(pick_table,))     
+            pick_table["output_table"] = output_table
+            col1, col2, __ = st.columns((2, 2, 20))
+            col1.button("Save", key="enrich", on_click=self.enrich, args=(pick_table,))
 
             if col2.button("Schedule"):
                 st.session_state.pick_table = pick_table
                 with st.form("Schedule Task"):
                     st.text_input("Task Name", key="task_name")
-                    st.selectbox("Warehouse Size", ["SMALL","MEDIUM","LARGE","XLARGE","XXLARGE"], key="task_wh_size")
-                    st.selectbox("Schedule", ["Hourly", "Daily", "Weekly", "Monthly"], key="task_schedule")
-                    st.form_submit_button("Save Task", on_click = self.save_and_schedule)
+                    st.selectbox(
+                        "Warehouse Size",
+                        ["SMALL", "MEDIUM", "LARGE", "XLARGE", "XXLARGE"],
+                        key="task_wh_size",
+                    )
+                    st.selectbox(
+                        "Schedule",
+                        ["Hourly", "Daily", "Weekly", "Monthly"],
+                        key="task_schedule",
+                    )
+                    st.form_submit_button("Save Task", on_click=self.save_and_schedule)
 
         st.warning("All saved data will appear under ENRICHMENT.DATA")
 
@@ -304,11 +341,15 @@ class page_two(Base_Page):
     def print_sidebar(self):
         with st.sidebar:
             super().print_sidebar()
-            st.button("Go to main",key='sidebar button 2', on_click=set_page,args=('main',))
+            st.button(
+                "Go to main", key="sidebar button 2", on_click=set_page, args=("main",)
+            )
+
 
 class page_three(Base_Page):
     def __init__(self):
-        self.name="three"
+        self.name = "three"
+
     def print_page(self):
         super().print_page()
         enriched_data = st.session_state.enriched_data
@@ -317,20 +358,25 @@ class page_three(Base_Page):
     def print_sidebar(self):
         with st.sidebar:
             super().print_sidebar()
-            st.button("Go to main",key='sidebar button 2', on_click=set_page,args=('main',))
+            st.button(
+                "Go to main", key="sidebar button 2", on_click=set_page, args=("main",)
+            )
+
 
 if "session" not in st.session_state:
     st.session_state.session = get_active_session()
 
 if "page" not in st.session_state:
-    st.session_state.page="main"
+    st.session_state.page = "main"
 
-pages = [main_page(),settings_page(),table_mapping_page(),page_two(),page_three()]
+pages = [main_page(), settings_page(), table_mapping_page(), page_two(), page_three()]
+
 
 def main():
     for page in pages:
         if page.name == st.session_state.page:
-            page.print_page();
-            page.print_sidebar();
+            page.print_page()
+            page.print_sidebar()
+
 
 main()
